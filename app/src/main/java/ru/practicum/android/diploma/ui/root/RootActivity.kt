85e809218b3297c2c43083ca1728afb87fc04595
OlgaRumRum <Olga.tyuren@yandex.ru>
2024-12-08 08:26:21 +0300

    JF-3: Add FragmentContainerView

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -3,12 +3,19 @@ package ru.practicum.android.diploma.ui.root
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.practicum.android.diploma.BuildConfig
-import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+
+    private var _binding: ActivityRootBinding? = null
+    private val binding get() = requireNotNull(_binding) { "Binding is not initialized" }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+
+        _binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
