bf9df2277f0cf1828e7e2d9506138fa2ddef963e
Vladislav Doronin <mysterion1337@yandex.ru>
2024-12-10 19:52:22 +0300

    JF-20: убрал всю реализацию адаптера чтобы детект не ругался

--- app/src/main/java/ru/practicum/android/diploma/presentation/vacancy_search/OnItemClickListener.kt
+++
@@ -1,7 +0,0 @@
-package ru.practicum.android.diploma.presentation.vacancy_search
-
-import ru.practicum.android.diploma.domain.models.JobItem
-
-fun interface OnItemClickListener {
-    fun onItemClick(item: JobItem)
-}
