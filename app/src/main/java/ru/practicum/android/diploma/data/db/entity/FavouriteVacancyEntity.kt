0fbf2c1095fe9495007859cd4115434e13cde331
Evgeniy <none>
2024-12-15 00:09:52 +0300

    JF-28: Add properties to FavouriteVacancyEntity

--- app/src/main/java/ru/practicum/android/diploma/data/db/entity/FavouriteVacancyEntity.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/entity/FavouriteVacancyEntity.kt
@@ -7,4 +7,21 @@ import androidx.room.PrimaryKey
 data class FavouriteVacancyEntity(
     @PrimaryKey
     val vacancyId: String,
+    val name: String,
+    val city: String,
+    val employerName: String?,
+    val employerLogoUrl: String?,
+    val employment: String?,
+    val experience: String?,
+    val salary: String,
+    val contactsName: String?,
+    val contactsEmail: String?,
+    val contactsPhone: String?,
+    val description: String,
+    val keySkills: String,
+    val professionalRoles: String,
+    val schedule: String?,
+    val workingDays: String?,
+    val workingTimeIntervals: String?,
+    val workingTimeModes: String?,
 )
