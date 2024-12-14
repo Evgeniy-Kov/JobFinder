bb08748cf1cd7a1b04674a00c2b0aea6fe092e42
Evgeniy <none>
2024-12-15 00:23:22 +0300

    JF-28: Add VacancyDetailsDto to VacancyDetails mapper

--- app/src/main/java/ru/practicum/android/diploma/util/Mapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/Mapper.kt
@@ -1,8 +1,10 @@
 package ru.practicum.android.diploma.util
 
 import android.content.Context
+import ru.practicum.android.diploma.data.dto.VacancyDetailsDto
 import ru.practicum.android.diploma.data.dto.VacancyDto
 import ru.practicum.android.diploma.domain.models.Vacancy
+import ru.practicum.android.diploma.domain.models.VacancyDetails
 
 fun VacancyDto.toVacancy(context: Context): Vacancy {
     return Vacancy(
@@ -12,5 +14,28 @@ fun VacancyDto.toVacancy(context: Context): Vacancy {
         employerName = employer.name,
         employerLogoUrl = employer.logoUrls?.original,
         salary = getFormattedSalary(salary, context)
+    )
+}
+
+fun VacancyDetailsDto.toVacancyDetails(context: Context): VacancyDetails {
+    return VacancyDetails(
+        id = id,
+        name = name,
+        city = area.name,
+        employerName = employer?.name,
+        employerLogoUrl = employer?.logoUrls?.original,
+        salary = getFormattedSalary(salary, context),
+        employment = employment?.name,
+        experience = experience?.name,
+        contactsName = contacts?.name,
+        contactsEmail = contacts?.email,
+        contactsPhone = contacts?.phones?.first()?.formatted,
+        description = description,
+        professionalRoles = professionalRoles.joinToString("\n"){it.name},
+        keySkills = keySkills.joinToString("\n"){it.name},
+        schedule = schedule?.name,
+        workingDays = workingDays?.joinToString("\n"){it.name},
+        workingTimeIntervals = workingTimeIntervals?.joinToString("\n"){it.name},
+        workingTimeModes = workingTimeModes?.joinToString("\n"){it.name}
     )
 }
