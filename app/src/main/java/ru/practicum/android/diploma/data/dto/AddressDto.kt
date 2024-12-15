1242a5f000249ed4cd1dc2731a62d6f4b829ede3
Evgeniy <none>
2024-12-15 12:41:29 +0300

    JF-32: Bring AddressDto class to a unified format

--- app/src/main/java/ru/practicum/android/diploma/data/dto/AddressDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/AddressDto.kt
@@ -1,7 +1,15 @@
 package ru.practicum.android.diploma.data.dto
 
+import com.google.gson.annotations.SerializedName
+
+/**
+ * @param city `Nullable` Хранит информацию о названии города
+ * @param street `Nullable` Хранит информацию о названии улицы
+ * @param building `Nullable` Хранит информацию о номере дома
+ */
+
 data class AddressDto(
-    val city: String?,
-    val street: String?,
-    val building: String?
+    @SerializedName("city") val city: String?,
+    @SerializedName("street") val street: String?,
+    @SerializedName("building") val building: String?
 )
