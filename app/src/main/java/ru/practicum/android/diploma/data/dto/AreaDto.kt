b4327222af63bc1835f7f7fe7ad7be81657f9967
Evgeniy <none>
2024-12-15 12:17:48 +0300

    JF-32: Reformat code in AreaDto class

--- app/src/main/java/ru/practicum/android/diploma/data/dto/AreaDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/AreaDto.kt
@@ -3,8 +3,8 @@ package ru.practicum.android.diploma.data.dto
 import com.google.gson.annotations.SerializedName
 
 data class AreaDto(
-    @SerializedName("parent_id")val parentId: String,
-    @SerializedName("id")val id: String,
-    @SerializedName("name")val name: String,
-    @SerializedName("areas")val areas: List<AreaDto>
+    @SerializedName("parent_id") val parentId: String,
+    @SerializedName("id") val id: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("areas") val areas: List<AreaDto>
 )
