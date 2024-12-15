70248519282ddc8f21f4aea999ea16c1e7d1ec5d
Roman <mih-xrom@ya.ru>
2024-12-15 21:12:20 +0300

    JF-35: Add init to DI

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -1,7 +1,11 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.repository.FavouriteVacancyRepositoryImpl
+import ru.practicum.android.diploma.domain.api.FavouriteVacancyRepository
 
 val repositoryModule = module {
-
+    single<FavouriteVacancyRepository> {
+        FavouriteVacancyRepositoryImpl(get(), get())
+    }
 }
