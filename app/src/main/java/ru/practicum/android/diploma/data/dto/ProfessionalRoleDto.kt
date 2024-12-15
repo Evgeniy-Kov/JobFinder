b2dbea390b1b3543067fc5b1245b12944e3a7517
Evgeniy <none>
2024-12-15 17:10:45 +0300

    JF-32: Fix typo

--- app/src/main/java/ru/practicum/android/diploma/data/dto/ProfessionalRoleDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/ProfessionalRoleDto.kt
@@ -3,7 +3,7 @@ package ru.practicum.android.diploma.data.dto
 import com.google.gson.annotations.SerializedName
 
 /**
- *  @param name Хранит информацию о название профессиональной роли
+ *  @param name Хранит информацию о названии профессиональной роли
  */
 
 data class ProfessionalRoleDto(
