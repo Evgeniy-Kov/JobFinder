92c3a78e7e46def73eb7541f138891c7193b023b
Evgeniy <none>
2024-12-19 10:09:52 +0300

    JF-37: Reformat code

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -1,7 +1,7 @@
 package ru.practicum.android.diploma.di
 
-import org.koin.core.module.dsl.viewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.ui.search.VacancySearchViewModel
 import ru.practicum.android.diploma.ui.vacancy.VacancyDetailsViewModel
