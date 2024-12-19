45c8783697406e9472ac53c6a139b97e7bfdd7cd
Evgeniy <none>
2024-12-19 21:43:04 +0300

    JF-54: Add PagingSourceInteractor and PagingSourceRepository interfaces

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/PagingSourceRepository.kt
@@ -0,0 +1,9 @@
+package ru.practicum.android.diploma.domain.api
+
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.models.Vacancy
+
+interface PagingSourceRepository {
+    fun getPagedData(query: String): Flow<PagingData<Vacancy>>
+}
