fc1dff7db5a9889f400fc03d8af5fec0365ca1ae
Evgeniy <none>
2024-12-20 15:58:15 +0300

    JF-57: Reformat code

--- app/src/main/java/ru/practicum/android/diploma/util/Formatter.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/Formatter.kt
@@ -3,7 +3,6 @@ package ru.practicum.android.diploma.util
 import android.content.Context
 import ru.practicum.android.diploma.R
 
-
 fun getFormattedSalary(salaryFrom: Int?, salaryTo: Int?, currency: String?, context: Context): String {
     return if (salaryFrom != null || salaryTo != null) {
         var result = ""
@@ -13,8 +12,9 @@ fun getFormattedSalary(salaryFrom: Int?, salaryTo: Int?, currency: String?, cont
         if (salaryTo != null) {
             result += String.format(context.getString(R.string.salary_to), salaryTo)
         }
-        if (currency != null)
+        if (currency != null) {
             result += currency
+        }
         result
     } else {
         context.getString(R.string.salary_not_specified)
