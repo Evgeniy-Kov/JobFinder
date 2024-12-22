713f20911ea8bfffcd3db5a298ab28a7688a0ee7
Vladislav Doronin <mysterion1337@yandex.ru>
2024-12-22 20:13:55 +0300

    JF-68: убрал лишний пробел

--- app/src/main/java/ru/practicum/android/diploma/domain/api/IndustriesRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/IndustriesRepository.kt
@@ -3,7 +3,6 @@ package ru.practicum.android.diploma.domain.api
 import ru.practicum.android.diploma.domain.models.Industry
 import ru.practicum.android.diploma.domain.models.Resource
 
-
 interface IndustriesRepository {
     suspend fun getIndustries(query: String): Resource<List<Industry>>
 }
