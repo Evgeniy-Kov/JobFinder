dee07aff4d87e7768b87961a6c82c042077e32cd
Evgeniy <none>
2024-12-25 22:34:58 +0300

    JF-87: Add AreaScreenState sealed interface

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/region/AreaScreenState.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.ui.region
+
+sealed interface AreaScreenState {
+    data object Loading : AreaScreenState
+    data object Content : AreaScreenState
+    data class Error(val message: Int) : AreaScreenState
+}
